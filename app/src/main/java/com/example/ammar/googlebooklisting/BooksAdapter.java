bdae1106f84c862e5fdccdc9d8586761b996c811
Ammar <plus.ammaryasser@gmail.vom>
2017-06-20 08:29:26 +0200

    first commit

--- app/src/main/java/com/example/ammar/googlebooklisting/BooksAdapter.java
+++ app/src/main/java/com/example/ammar/googlebooklisting/BooksAdapter.java
@@ -1,4 +1,24 @@
 package com.example.ammar.googlebooklisting;
 
-public class BooksAdapter {
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class BooksAdapter extends ArrayAdapter<book> {
+
+    public BooksAdapter(@NonNull Context context, @NonNull List<book> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
